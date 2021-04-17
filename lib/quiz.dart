ae4106d294b9bfd50d907a711b01faa9f37aa58e
aLAA-9 <74575963+aLAA-9@users.noreply.github.com>
2021-04-18 01:39:55 +0200

    Add files via upload

---
+++ lib/quiz.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  @override
+  _QuizState createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text(
+              'Quiz',
+              style: TextStyle(
+                  fontSize: 20.0, fontFamily: 'Lobster', color: Colors.black87),
+              textAlign: TextAlign.center,
+            )));
+  }
+}
